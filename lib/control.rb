ea93e886b60d38ed41b81331c7930fe74e57897f
Ohad Partuck 1 <ohadpartuck@gmail.com>
2015-02-13 21:43:11 +0200

    few tweeks

--- lib/control.rb
+++ lib/control.rb
@@ -2,15 +2,14 @@ require 'socket'
 require 'control_helper'
 
 class Control
-	extend self
 
-	def do(action: nil,
-				 start_command: nil,
-				 kill_command: nil,
-				 port: nil,
-				 app_name: nil,
-				 env: nil,
-				 app_file_name = nil)
+	def self.do(action: nil,
+			 start_command: nil,
+			 kill_command: nil,
+			 port: nil,
+			 app_name: nil,
+			 env: nil,
+			 app_file_name: nil)
 
 		action 				= action.to_s
 		hostname 			= Socket.gethostname
@@ -27,24 +26,24 @@ class Control
 		end
 	end
 
-	def helper
+	def self.helper
 		ControlHelper
 	end
 
-	def restart_actions(hostname, port, app_name, app_file_name)
+	def self.restart_actions(hostname, port, app_name, app_file_name)
 		helper.kill_the_old_process_if_needed(port, app_name, app_file_name)
 
 		new_pid = helper.start_a_new_process!(environment, port, app_file_name)
 		helper.check_for_success_in_starting_new_process!(hostname, new_pid)
 	end
-	def start_actions(hostname, app_name, port, app_file_name)
+	def self.start_actions(hostname, app_name, port, app_file_name)
 		helper.exit_if_old_process_is_already_running!(app_name, port)
 
 		new_pid = helper.start_a_new_process!(environment, port, app_file_name)
 		helper.check_for_success_in_starting_new_process!(hostname, new_pid)
 	end
 
-	def stop_actions(app_name, port)
+	def self.stop_actions(app_name, port)
 		old_pid = helper.pull_old_pid_and_exit_if_not_running!(app_name, port)
 		helper.kill_the_process!(old_pid)
 	end
